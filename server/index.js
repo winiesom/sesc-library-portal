289018afc0d433c3f6a923807273850320482bab
mmaesom <winifred.esomonu@stetis.com>
2023-04-22 05:50:27 +0100

    allowed origin on cors

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const app = express();
 
 let corsOptions = {
-origin: "http://localhost:8081"
+    origin: "*"
+// origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
